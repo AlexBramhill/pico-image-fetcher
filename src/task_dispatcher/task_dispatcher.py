90ef93c33a7cb70f4e9a926c17d2dae282c3e4f4
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-06 01:02:35 +0100

    fix double running of dispatcher

--- src/task_dispatcher/task_dispatcher.py
+++ src/task_dispatcher/task_dispatcher.py
@@ -4,33 +4,22 @@ from src.task_scheduler.task_scheduler import TaskScheduler
 
 class TaskDispatcher:
     def __init__(self, scheduler: TaskScheduler):
-        self.scheduler = scheduler
+        self._scheduler = scheduler
 
     def dispatch(self):
-        task_generator = self.scheduler.yield_most_overdue_task()
-        task = next(task_generator)
-
+        task = self._scheduler.pop_overdue_task()
         if task is None:
             return
 
         try:
+            print(f"Running task: {task.name}")
             task.run()
         except Exception as e:
             print(f"Error in task {task.name}: {e}")
 
         if not task.should_run_once:
             now = time.ticks_ms()
-
             last_run = task.last_run_in_epoch if task.last_run_in_epoch is not None else now
-            print(
-                f"Task {task.name} previous last run at {task.last_run_in_epoch}, now at {last_run}")
-            print(
-                f"Task {task.name} next run in {task.get_ms_to_next_run()} ms")
-            task.next_run_in_epoch = last_run + task.get_ms_to_next_run()
-            print(f"Task {task.name} next run at {task.next_run_in_epoch}")
-            task.last_run_in_epoch = now
-            print(f"Task {task.name} last run at {task.last_run_in_epoch}")
-
-            print("Adding task back to the scheduler")
-            self.scheduler.add_task(task)
-            print(f"Task {task.name} added back to the scheduler")
+            task.next_run_in_epoch = now + task.get_ms_to_next_run()
+            task.last_run_in_epoch = last_run
+            self._scheduler.add_task(task)
--- src/task_scheduler/task_scheduler.py
+++ src/task_scheduler/task_scheduler.py
@@ -6,20 +6,17 @@ class TaskScheduler:
     def __init__(self, tasks=[]):
         self.tasks = tasks
 
-    def yield_most_overdue_task(self):
+    def pop_overdue_task(self):
         now = time.ticks_ms()
         overdue_tasks = [
             task for task in self.tasks if task.next_run_in_epoch is None or task.next_run_in_epoch <= now]
         if overdue_tasks:
-            print(f"Found {len(overdue_tasks)} overdue tasks.")
             oldest_task = min(
                 overdue_tasks, key=lambda t: t.next_run_in_epoch if t.next_run_in_epoch is not None else float('-inf'))
             self.tasks.remove(oldest_task)
-            print(
-                f"Yielding most overdue task: {oldest_task.name}, last run at {oldest_task.last_run_in_epoch}, next run at {oldest_task.next_run_in_epoch}")
-            yield oldest_task
+            return oldest_task
         else:
-            yield None
+            return None
 
     def add_task(self, task: Task):
         if not isinstance(task, Task):
